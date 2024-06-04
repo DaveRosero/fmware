8d8a72dad0a9f8dcf80f000ef4dbb0b36233dda7
MarcJhero <marcjhero.dev@gmail.com>
2024-06-04 21:28:02 +0800

    Update pos.php

--- views/pos/pos.php
+++ views/pos/pos.php
@@ -149,7 +149,7 @@ $stmt->bind_result($unit_price, $image, $name, $barcode, $unit_value, $qty, $id,
           </div>
           <h2 class="text-end" id="cart-total">Total: ₱0</h2>
           <div class="d-grid gap-2">
-            <button class="btn btn-danger reset-cart">TESTING</button>
+            <button class="btn btn-danger reset-cart">Clear</button>
             <button class="btn btn-success print">Print</button>
           </div>
         </div>
