a1f72f4467f38144280742bed040e67fa58f9a45
DaveRosero <125649484+DaveRosero@users.noreply.github.com>
2024-06-05 14:23:17 +0800

    added discount and refunds in dashboard
    added crucial information available in navbar for easy access

--- views/admin/content/dashboard-content.php
+++ views/admin/content/dashboard-content.php
@@ -21,7 +21,7 @@
             <div class="card">
                 <div class="card-strip bg-danger"></div>
                 <div class="card-body">
-                    <h3>Expenses</h3>
+                    <h3>Wages & Expenses</h3>
                     <div class="row  align-items-center">
                         <div class="col-6">
                             <h5 class="text-center">P1,269,420.00</h5>
@@ -53,9 +53,49 @@
     </div>
 
     <div class="row">
-        <div class="card">
-            <div class="card-body">
-                <div id="profits-chart"></div>
+        <div class="col-4">
+            <div class="card">
+                <div class="card-strip bg-secondary"></div>
+                <div class="card-body">
+                    <h3>Discounts</h3>
+                    <div class="row  align-items-center">
+                        <div class="col-6">
+                            <h5 class="text-center">P1,269,420.00</h5>
+                        </div>
+                        <div class="col-6 d-flex align-items-center justify-content-center">
+                            <i class="fas fa-solid fa-tags text-secondary" style="font-size: 6rem;"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-strip bg-warning"></div>
+                <div class="card-body">
+                    <h3>Refund</h3>
+                    <div class="row  align-items-center">
+                        <div class="col-6">
+                            <h5 class="text-center">P1,269,420.00</h5>
+                        </div>
+                        <div class="col-6 d-flex align-items-center justify-content-center">
+                            <i class="fas fa-solid fa-sync-alt text-warning" style="font-size: 6rem;"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-8">
+            <div class="card">
+                <div class="card-body">
+                    <div class="text-end">
+                        <select class="btn btn-sm text-dark fw-semibold" name="" id="">
+                            <option value ="daily">Daily</option>
+                            <option value="weekly">Weekly</option>
+                            <option value="monthly">Monthly</option>
+                            <option value="yearly">Yearly</option>
+                        </select>
+                    </div>
+                    <div id="profits-chart"></div>
+                </div>
             </div>
         </div>
     </div>
