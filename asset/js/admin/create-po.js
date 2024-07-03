535d0f33403830af3809e09c2e9d364f2e41e559
DaveRosero <125649484+DaveRosero@users.noreply.github.com>
2024-07-04 00:40:58 +0800

    supplier products dropdown select form in po

---
+++ asset/js/admin/create-po.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#product').select2({
+        width: '100%',
+        placeholder: 'Select a product',
+        theme: 'bootstrap-5',
+        escapeMarkup: function(markup) {
+            return markup;
+        }
+    });
+});
