10f6014e44d3caff8bd1e0d3423a6e116e770187
Israel <gonzalesisraeljosepha.pdm@gmail.com>
2024-07-08 17:50:49 +0800

    7/8/24 update code
    updating na ng data pabalik sa database existed isa-isahin ko yung missing post variable ulit kung san pwede ko sila ilalagay dun sa frontend inayos ko mga need ayosin like scroll sa modal, subtotal sa pagdisplay ng product sa history makikita na din at dinagdag ko yung discount para same sila ng checkout para hindi malito
    
    PS:first encounter yung user_id dun ako magsisimula tomorrow

--- views/pos/model/history_product.php
+++ views/pos/model/history_product.php
@@ -11,6 +11,7 @@ $mysqli = database();
 $pos_ref = $mysqli->real_escape_string($_GET['pos_ref']);
 
 $query = "SELECT  product.name AS product_name,
+                  product.unit_value AS product_uvalue,
                   unit.name AS unit_name,
                   variant.name AS variant_name,
                   price_list.unit_price AS product_price,
@@ -36,20 +37,17 @@ if (!$result) {
 $content = '';
 
 while ($row = $result->fetch_assoc()) {
-
+    $subtotal = $row['product_price'] * $row['product_qty'];
     $content .= '<tr>
-    <td>' . $row['product_name'] . '</td>
-    <td>' . $row['unit_name'] . '</td>
-    <td>' . $row['variant_name'] . '</td>
-    <td>' . $row['product_price'] . '</td>
-    <td>' . $row['product_qty'] . '</td>
-    <td>
-    </td>
+    <td cclass="text-center">' . $row['product_name'] . '</td>
+    <td class="text-center">' . $row['product_uvalue'] . ' ' . $row['unit_name'] . '</td>
+    <td class="text-center">' . $row['variant_name'] . '</td>
+    <td class="text-center">₱' . number_format($row['product_price'], 2) . '</td>
+    <td class="text-center">' . $row['product_qty'] . '</td>
+    <td class="text-center">₱' . number_format($subtotal, 2) . '</td>
     </tr>';
 }
 echo $content;
-
-
 
 // Close the connection
 $mysqli->close();
