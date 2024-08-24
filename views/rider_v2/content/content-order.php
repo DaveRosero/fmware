bf5ccb7f6070a56829683760cc85de78f8f367ab
MarcJhero <156150375+MarcJhero@users.noreply.github.com>
2024-08-24 13:49:56 +0800

    Update content-order.php

--- views/rider_v2/content/content-order.php
+++ views/rider_v2/content/content-order.php
@@ -12,25 +12,6 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
-                    <div class="d-flex justify-content-between">
-                        <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-outline-success" type="submit">Search</button>
-                        </form>
-                        <div class="dropdown">
-                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Sort By
-                            </button>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Order Ref</a></li>
-                                <li><a class="dropdown-item" href="#">Date</a></li>
-                                <li><a class="dropdown-item" href="#">Paid</a></li>
-                                <li><a class="dropdown-item" href="#">Delivery</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12">
                     <div id="orders-container" class="mt-3">
                     </div>
                 </div>
