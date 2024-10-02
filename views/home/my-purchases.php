982f3eb375bdf63d6ff26b9eb7622e9bf5ff273f
DaveRosero <125649484+DaveRosero@users.noreply.github.com>
2024-10-02 16:19:20 +0800

    view cancelled orders function done

--- views/home/my-purchases.php
+++ views/home/my-purchases.php
@@ -1,49 +1,52 @@
 <?php
-    include_once 'session.php';
-    require_once 'model/user/user.php';
-    require_once 'model/home/home.php';
-    require_once 'model/home/cartClass.php';
-    require_once 'model/user/transactionClass.php';
-    require_once 'model/admin/orderClass.php';
+include_once 'session.php';
+require_once 'model/user/user.php';
+require_once 'model/home/home.php';
+require_once 'model/home/cartClass.php';
+require_once 'model/user/transactionClass.php';
+require_once 'model/admin/orderClass.php';
 
-    $home = new Home();
-    $user = new User();
-    $cart = new Cart();
-    $transaction = new Transaction();
-    $order = new Order();
+$home = new Home();
+$user = new User();
+$cart = new Cart();
+$transaction = new Transaction();
+$order = new Order();
 
-    if (isset($_SESSION['group']) && isset($_SESSION['user_id']) && $_SESSION['group'] !== 'user') {
-        $home->redirectUser($_SESSION['group']);
-    }
-    $user_info = $user->getUser($_SESSION['email'] ?? null);
-    $statusType = array('to-pay', 'pending', 'to-receive', 'delivered', 'completed', 'cancelled');
-    if (!in_array($status, $statusType)) {
-        header('Location: /404');
-    }
+if (isset($_SESSION['group']) && isset($_SESSION['user_id']) && $_SESSION['group'] !== 'user') {
+    $home->redirectUser($_SESSION['group']);
+}
+$user_info = $user->getUser($_SESSION['email'] ?? null);
+$statusType = array('to-pay', 'pending', 'to-receive', 'delivered', 'completed', 'cancelled');
+if (!in_array($status, $statusType)) {
+    header('Location: /404');
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>FMWare</title>
-        <link rel="icon" href="/asset/images/store/logo.png" type="image/png">
-        <?php 
-            require_once 'config/load_vendors.php'; 
-        ?>
-        <link rel="stylesheet" href="/asset/css/index.css">
-    </head>
-    <body>
-        <?php include_once 'views/home/template/header.php'; ?>
 
-        <?php include_once 'views/home/content/my-purchases-content.php'; ?>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>FMWare</title>
+    <link rel="icon" href="/asset/images/store/logo.png" type="image/png">
+    <?php
+    require_once 'config/load_vendors.php';
+    ?>
+    <link rel="stylesheet" href="/asset/css/index.css">
+</head>
 
-        <?php include_once 'views/home/modals/my-purchases-modal.php'; ?>
-            
-        <script src="/vendor/jQueryQr/src/jquery.qrcode.js"></script>
-        <script src="/vendor/jQueryQr/src/qrcode.js"></script>
-        <script src="/vendor/NotifyJS/js/notify.js"></script>
-        <script src="/asset/js/home/my-purchases.js"></script>
-        <script src="/asset/js/home/home.js"></script>
-    </body>
+<body>
+    <?php include_once 'views/home/template/header.php'; ?>
+
+    <?php include_once 'views/home/content/my-purchases-content.php'; ?>
+
+    <?php include_once 'views/home/modals/my-purchases-modal.php'; ?>
+
+    <script src="/vendor/jQueryQr/src/jquery.qrcode.js"></script>
+    <script src="/vendor/jQueryQr/src/qrcode.js"></script>
+    <script src="/vendor/NotifyJS/js/notify.js"></script>
+    <script src="/asset/js/home/my-purchases.js"></script>
+    <script src="/asset/js/home/home.js"></script>
+</body>
+
 </html>
