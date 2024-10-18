5234fcaff4ac4f99bdfd1eee7c950ad2f7b44d42
MarcJhero <156150375+MarcJhero@users.noreply.github.com>
2024-10-18 10:15:08 +0800

    Update home UI

--- views/home/template/header.php
+++ views/home/template/header.php
@@ -6,39 +6,41 @@
             FMWare
         </a>
 
-        <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#templatemo_main_nav" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#templatemo_main_nav" aria-controls="templatemo_main_nav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="align-self-center collapse navbar-collapse flex-fill  d-lg-flex justify-content-lg-between" id="templatemo_main_nav">
-            <div class="flex-fill">
-                <ul class="nav navbar-nav d-flex justify-content-between mx-lg-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/about">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/shop/all">Shop</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/contact">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="navbar align-self-center d-flex">
+        <div class="collapse navbar-collapse flex-fill d-lg-flex justify-content-lg-between align-items-center" id="templatemo_main_nav">
+            <ul class="navbar-nav mx-lg-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/about">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/shop/all">Shop</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/contact">Contact</a>
+                </li>
+            </ul>
+
+            <div class="navbar d-flex align-items-center">
                 <?php if ($user->isLoggedIn()): ?>
+                    <!-- Cart Icon with Cart Count -->
                     <a class="nav-icon position-relative text-decoration-none" id="cart" href="/cart">
                         <i class="fa fa-fw fa-cart-arrow-down text-dark mr-1"></i>
-                        <span class="position-absolute top-0 left-100 translate-middle badge rounded-pill bg-light text-dark" id="cart-count">
+                        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-light text-dark" id="cart-count">
                             <?php
-                                $cart_count = $cart->cartCount($_SESSION['user_id']);
-                                echo $cart_count;
+                            $cart_count = $cart->cartCount($_SESSION['user_id']);
+                            echo $cart_count;
                             ?>
                         </span>
                     </a>
-                    <div class="dropdown">
+
+                    <!-- User Dropdown for Account and Logout -->
+                    <div class="dropdown ms-3">
                         <a class="nav-icon position-relative text-decoration-none dropdown-toggle" href="#" role="button" id="userDropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <i class="fa fa-fw fa-user text-dark mr-3"></i>
                         </a>
@@ -51,17 +53,17 @@
                         </div>
                     </div>
 
-                <?php elseif (!$user->isLoggedIn()): ?>
-                    <a class="nav-icon d-none d-lg-inline text-decoration-none" href="/login">
+                <?php else: ?>
+                    <!-- Login and Register Buttons for Unauthenticated Users -->
+                    <a class="nav-icon text-decoration-none d-lg-inline-block" href="/login">
                         <i class="fas fa-sign-in-alt text-dark mr-2"></i> Login
                     </a>
-                    <a class="nav-icon d-none d-lg-inline text-decoration-none" href="/register">
+                    <a class="nav-icon text-decoration-none d-lg-inline-block ms-3" href="/register">
                         <i class="fas fa-user-plus text-dark mr-2"></i> Register
                     </a>
                 <?php endif; ?>
             </div>
         </div>
-
     </div>
 </nav>
 <!-- Close Header -->
